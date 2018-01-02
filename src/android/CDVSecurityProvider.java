d2d9be5037608fdbb60b87d528335daf11acf041
Mariano Botta <marianoabotta@gmail.com>
2018-01-02 18:12:05 -0300

    Fix errors

--- src/android/CDVSecurityProvider.java
+++ src/android/CDVSecurityProvider.java
@@ -13,6 +13,17 @@ import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.security.ProviderInstaller;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.BroadcastReceiver;
+
 
 import android.util.Log;
 
@@ -29,6 +40,10 @@ public class CDVSecurityProvider extends CordovaPlugin {
                     new java.util.HashMap<String,BroadcastReceiver>(10);
 
 
+    private Context getApplicationContext() {
+        return this.cordova.getActivity().getApplicationContext();
+    }
+
     /**
      *
      * @param action          The action to execute.
@@ -44,17 +59,17 @@ public class CDVSecurityProvider extends CordovaPlugin {
             final String eventName = args.getString(0);
 
             try {
-                ProviderInstaller.installIfNeeded(this.cordova.getActivity().getApplicationContext());
+                ProviderInstaller.installIfNeeded(this.getApplicationContext());
                 Log.d(LOG_TAG, "ProviderInstaller Update Success");
             } catch (GooglePlayServicesRepairableException e) {
 
                 // Indicates that Google Play services is out of date, disabled, etc.
 
                 // Prompt the user to install/update/enable Google Play services.
-                GooglePlayServicesUtil.showErrorNotification(e.getConnectionStatusCode(), getContext());
+                GooglePlayServicesUtil.showErrorNotification(e.getConnectionStatusCode(), this.getApplicationContext());
 
                 // Notify the SyncManager that a soft error occurred.
-                syncResult.stats.numIOExceptions++;
+                // syncResult.stats.numIOExceptions++;
 
                 Log.d(LOG_TAG, "ProviderInstaller failed: GooglePlayServicesRepairableException");
                 return false;
@@ -64,7 +79,7 @@ public class CDVSecurityProvider extends CordovaPlugin {
                 // to install an up-to-date Provider.
 
                 // Notify the SyncManager that a hard error occurred.
-                syncResult.stats.numAuthExceptions++;
+                // syncResult.stats.numAuthExceptions++;
 
                 Log.d(LOG_TAG, "ProviderInstaller failed: GooglePlayServicesNotAvailableException");
                 return false;
